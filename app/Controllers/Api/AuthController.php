1de2d7551aed0f1c89195b30b3d29d77cd00ff96
Raul Alvarez <raul.alva1926@gmail.com>
2023-02-09 12:30:42 -0600

    Creating Register endpoint

--- app/Controllers/Api/AuthController.php
+++ app/Controllers/Api/AuthController.php
@@ -2,15 +2,52 @@
 
 namespace App\Controllers\Api;
 
+use CodeIgniter\API\ResponseTrait;
+use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\RESTful\ResourceController;
+use CodeIgniter\Shield\Entities\User;
 use CodeIgniter\Shield\Models\UserModel;
 
 class AuthController extends ResourceController
 {
+    use ResponseTrait;
+
     // Register endpoint
     public function register()
     {
-        // Add users into application
+        $rules = [
+            "username" => "required|is_unique[users.username]",
+            "email" => "required|is_unique[auth_identities.secret]",
+            "password" => "required"
+        ];
+
+        if (!$this->validate($rules)) {
+            $response = [
+                "status" => ResponseInterface::HTTP_INTERNAL_SERVER_ERROR,
+                "message" => $this->validator->getErrors(),
+                "error" => true,
+                "data" => []
+            ];
+
+            return $this->respond($response, ResponseInterface::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $userObjet = new UserModel();
+        $user = new User([
+            "username" => $this->request->getVar("username"),
+            "email" => $this->request->getVar("email"),
+            "password" => $this->request->getVar("password")
+        ]);
+
+        $userObjet->save($user);
+
+        $response = [
+            "status" => ResponseInterface::HTTP_OK,
+            "message" => "User created successfully",
+            "error" => false,
+            "data" => []
+        ];
+        return $this->respond($response, ResponseInterface::HTTP_OK);
     }
 
     // Login endpoint
